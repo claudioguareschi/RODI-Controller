3a63f76cddc59f2878837811a42219a396bbdfba
Claudio Guareschi <cguareschimd@gmail.com>
2020-02-22 07:39:06 -0500

    Just blink

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,24 @@
 / (C) 2020 Claudio Guareschi
 / V 0.00.01
 /
-/*****************************************************/
+*****************************************************/
 
 #include <Arduino.h>
 
+#include <ArduinoOTA.h>
+#include <FS.h>
+#include <SPIFFS.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+
+#include <ESPAsyncWebServer.h>
+#include <SPIFFSEditor.h>
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
-pinMode(LED_BUILTIN, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);
